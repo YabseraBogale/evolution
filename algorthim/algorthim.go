715f3ce24cc9d86aeacfd61205930646243f8b2f
YabseraBogale <yabserapython@gmail.com>
2026-09-09 18:57:45 +0300

    what is random sampling without replacement ?

---
+++ algorthim/algorthim.go
@@ -0,0 +1,28 @@
+package algorthim
+
+import "math/rand"
+
+func Sample[T any](src []T, k int)[]T{
+	if len(src)<k{
+		k=len(src)
+	}
+	indices:=rand.Perm(len(src))
+	result:=make([]T, k)
+	for i:=0;i<k;i++{
+		result[i]=src[indices[i]]
+	}
+	return result
+}
+
+func GenerateParent(length int64,geneSet[]string{}){
+	var gene []string{}
+	for {
+		if len(gene)>length{
+			break
+		} else{
+			sampleSize:=math.Min(length-(len(gene)),len(geneSet))
+			gene=append(gene, )
+		}
+		
+	}
+}
